7aa8f7c9a831062f7639fe7758bce242f242a119
philippA <erosiv01.begleitung@icloud.com>
2025-02-21 09:39:55 +0100

    Create const.py

---
+++ custom_components/advanced_snapshot/const.py
@@ -0,0 +1,5 @@
+DOMAIN = "advanced_snapshot"
+
+CONF_SNAPSHOT_FOLDER = "/config/www/snapshots"
+CONF_BACKUP_FOLDER = "/config/www/backupsnapshots/"
+CONF_FONT_FOLDER = "/config/custom_components/advanced_snapshot/fonts"
