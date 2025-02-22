700a34570a032cceae437b47a724f04b490603bd
Phil7989 <erosiv01.begleitung@icloud.com>
2025-02-23 00:22:15 +0100

    Automatisierter Commit von Home Assistant

--- custom_components/advanced_snapshot/const.py
+++ custom_components/advanced_snapshot/const.py
@@ -1,5 +1,1 @@
 DOMAIN = "advanced_snapshot"
-
-CONF_SNAPSHOT_FOLDER = "/config/www/snapshots"
-CONF_BACKUP_FOLDER = "/config/www/backupsnapshots/"
-CONF_FONT_FOLDER = "/config/custom_components/advanced_snapshot/fonts"
