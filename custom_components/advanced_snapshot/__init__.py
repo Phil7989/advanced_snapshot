97bf3f4378afc200cb30af669e2a45d65c61b604
philippA <erosiv01.begleitung@icloud.com>
2026-01-12 08:27:07 +0100

    Refactor os.makedirs call in backup process
    Remove 'exist_ok=True' from os.makedirs call.

--- custom_components/advanced_snapshot/__init__.py
+++ custom_components/advanced_snapshot/__init__.py
@@ -408,7 +408,7 @@ async def handle_record_video(hass: HomeAssistant, call: ServiceCall) -> Service
 
     if file_path_backup:
         try:
-            await hass.async_add_executor_job(os.makedirs, os.path.dirname(file_path_backup), exist_ok=True)
+            await hass.async_add_executor_job(os.makedirs, os.path.dirname(file_path_backup))
 
             # Use shutil.copy2 for a cleaner copy operation (also in the Executor)
             await hass.async_add_executor_job(shutil.copy2, file_path, file_path_backup)
